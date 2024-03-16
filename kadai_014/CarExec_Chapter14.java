a92c636d6c9fa17fd6b13ba4e6459187c73b0742
chirry-sketch <154793601+chirry-sketch@users.noreply.github.com>
2024-03-16 21:21:35 +0900

    14章課題
    課題提出

---
+++ kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,15 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+	public static void main(String[] args) {
+		
+		Car_Chapter14 gearChange = new Car_Chapter14( 1, 10);
+		
+		gearChange.gearChange(3);
+		gearChange.run();
+		
+	}
+}
+
+
+		
